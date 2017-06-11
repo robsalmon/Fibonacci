dee4344e03173434a8d7a1db9b171868c23e3a03
Robert Salmon <robdavidsalmon@msn.com>
2017-06-11 18:43:33 +0100

    Return the first three Fibonacci numbers with associated tests

---
+++ FibonacciProject.Tests/FibonacciUnitTests.cs
@@ -0,0 +1,61 @@
+using System;
+using Xunit;
+using FibonacciProject;
+
+namespace FibonacciTests
+{
+    public class FibonacciUnitTests
+    {
+        [Fact]
+        public void Fib_Given0_Return0()
+        {
+            //arrange
+            int n = 0;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Fib_Given1_Return1()
+        {
+            //arrange
+            int n = 1;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Given2_Return1()
+        {
+            //arrange
+            int n = 2;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fib_Given3_Return2()
+        {
+            //arrange
+            int n = 3;
+
+            //act
+            int result = Fibonacci.Fib(n);
+
+            //assert
+            Assert.Equal(2, result);
+        }
+    }
+}
---
+++ FibonacciProject/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FibonacciProject
+{
+    public class Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            } else if (n <= 2)
+            {
+                return 1;
+            }
+            return 2;
+        }
+    }
+}
