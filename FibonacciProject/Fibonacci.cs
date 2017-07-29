19fe68cecb402c5f1de95cf58ea84b0c87fd629a
Robert Salmon <robdavidsalmon@msn.com>
2017-07-29 10:47:19 +0100

    Original version

--- FibonacciProject/Fibonacci.cs
+++ FibonacciProject/Fibonacci.cs
@@ -13,7 +13,7 @@ namespace FibonacciProject
             {
                 return 1;
             }
-            return Fib(n-1) + Fib(n-2);
+            return Fib(n-1) + Fib(n-2); 
         }
     }
 }
